1a565742a521c955aede05f4f9899282d7e05597
Dipanshu <147638162+dipanshuuk711@users.noreply.github.com>
2024-09-07 08:56:31 +0530

    Update constants.js
    Check if if this fixes the error

--- Context/constants.js
+++ Context/constants.js
@@ -1,6 +1,6 @@
 ///0x5FbDB2315678afecb367f032d93F642f64180aa3
 // Ensure the path matches the actual location of NFTMarketplace.json
-import nftMarketplace from "../artifacts/NFTMarketplace.json";
+import nftMarketplace from "../contracts/NFTMarketplace.sol";
 
 
 export const NFTMarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
